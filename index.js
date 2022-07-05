396b00761663a2b6b7294e4c7d5dca1e4b86c70e
samabadvie <samabadvie@gmail.com>
2022-07-05 17:35:36 +0430

    Save data in object

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ async function main(){
 
     const $ = cheerio.load(html);
 
-    $('body > table > tbody > tr > td').each((index, element)=>{
-        console.log($(element).text());
+    let scrapedRows = [];
+
+    $('body > table > tbody > tr').each((index, element)=>{
+        if(index === 0) return true;
+
+        const tds = $(element).find("td");
+        const company = $(tds[0]).text();
+        const contact = $(tds[1]).text();
+        const country = $(tds[2]).text();
+        scrapedRows.push({company, contact, country});
     });
+
+    console.log(scrapedRows);
 }
 main();
